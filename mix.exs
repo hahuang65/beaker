c913d7f8fc9c111c9aeb1cd56e7929a36ab7bb16
Howard Huang <hao@hwrd.me>
2015-06-18 21:31:26 -0700

    Version 0.0.2
    Enhancements
    ------------
      * Beaker.TimeSeries implemented.
      * Beaker.MetricsController, Beaker.MetricsView and templates added.
      * Beaker.Counter.all/0 added.
      * Beaker.Counter.clear/0 added.
      * Beaker.Gauge.all/0 added.
      * Beaker.Gauge.clear/0 added.
    
    Bug Fixes
    ---------
      * None
    
    Deprecations
    ------------
      * None
    
    Backwards Incompatible Changes
    ------------------------------
      * None

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Beaker.Mixfile do
   def project do
     [
       app: :beaker,
-      version: "0.0.1",
+      version: "0.0.2",
       elixir: "~> 1.0",
       elixirc_paths: elixirc_paths(Mix.env),
       compilers: compilers(Mix.env),
