3e2915aa8d63d90b13d676786d4de42b59961551
Howard Huang <hao@hwrd.me>
2015-06-18 21:21:56 -0700

    Documentation for Beaker.TimeSeries.
    Documentation for Beaker.TimeSeries along with doc tests.
    Minor changes on the Beaker.TimeSeries tests.

--- lib/beaker/time_series.ex
+++ lib/beaker/time_series.ex
@@ -1,5 +1,15 @@
 defmodule Beaker.TimeSeries do
-  use GenServer
+  @moduledoc """
+  `Beaker.TimeSeries` is a simple time series. It's a metric that keeps track of values over time when sampled.
+
+  It is commonly used to keep track of changing values over time to look at patterns and averages
+
+  Examples are:
+    * Response time across time
+    * Download numbers over time
+    * Error rates across time
+
+  """
 
   ## Client API
 
@@ -8,22 +18,90 @@ defmodule Beaker.TimeSeries do
     GenServer.start_link(__MODULE__, :ok, name: :beaker_time_series)
   end
 
+  @doc """
+  Retrieves all time series in the form of a map.
+
+  ## Examples
+
+      iex> Beaker.TimeSeries.clear
+      :ok
+      iex> Beaker.TimeSeries.sample("all_series1", 10)
+      :ok
+      iex> Beaker.TimeSeries.sample("all_series2", 20)
+      :ok
+      iex> Beaker.TimeSeries.all |> Map.keys
+      ["all_series1", "all_series2"]
+
+  Returns `time_series` where time_series is a map of all the time_series currently existing.
+  """
   def all do
     GenServer.call(:beaker_time_series, :all)
   end
 
+  @doc """
+  Remove all time seriess currently stored in Beaker.
+
+  ## Examples
+
+      iex> Beaker.TimeSeries.sample("clear_series1", 10)
+      :ok
+      iex> Beaker.TimeSeries.sample("clear_series2", 20)
+      :ok
+      iex> Beaker.TimeSeries.all |> Enum.empty?
+      false
+      iex> Beaker.TimeSeries.clear
+      :ok
+      iex> Beaker.TimeSeries.all |> Enum.empty?
+      true
+
+  Returns `:ok`.
+  """
   def clear do
     GenServer.cast(:beaker_time_series, :clear)
   end
 
+  @doc """
+  Get the time series for the given key.
+
+  ## Parameters
+    * `key`: The name of the time_series to retrieve.
+
+  ## Examples
+
+      iex> Beaker.TimeSeries.get("get_series")
+      nil
+      iex> Beaker.TimeSeries.sample("get_series", 10)
+      :ok
+      iex> Beaker.TimeSeries.get("get_series") |> Enum.count
+      1
+
+  Returns `time_series` where time_series is a list of the samples taken for the given key.
+  """
   def get(key) do
     GenServer.call(:beaker_time_series, {:get, key})
   end
 
+  @doc """
+  Record a sample for the given key with the given value.
+
+  ## Parameters
+    * `key`: The name of the time_series to sample.
+    * `value`: The value to save for the sample.
+
+  ## Examples
+
+      iex> Beaker.TimeSeries.sample("set_series", 5)
+      :ok
+      iex> Beaker.TimeSeries.get("set_series") |> hd |> elem(1)
+      5
+
+  Returns `time_series` where time_series is a list of the samples taken for that key.
+  """
   def sample(key, value) do
     GenServer.cast(:beaker_time_series, {:sample, key, value})
   end
 
+  @doc false
   defp now_in_epoch do
     {mega, sec, micro} = :os.timestamp()
     (mega * 1000000 + sec) * 1000000 + micro
--- test/lib/beaker/time_series_test.exs
+++ test/lib/beaker/time_series_test.exs
@@ -11,19 +11,19 @@ defmodule Beaker.TimeSeriesTest do
 
   test "TimeSeries.all returns a map with all time series and values" do
     TimeSeries.clear
-    TimeSeries.sample("all_time_series1", 20)
-    TimeSeries.sample("all_time_series1", 55)
-    TimeSeries.sample("all_time_series2", 10)
-    TimeSeries.sample("all_time_series2", 45)
+    TimeSeries.sample("all1", 20)
+    TimeSeries.sample("all1", 55)
+    TimeSeries.sample("all2", 10)
+    TimeSeries.sample("all2", 45)
 
-    %{"all_time_series1" => [{_time1, 55}, {_time2, 20}], "all_time_series2" => [{_time3, 45}, {_time4, 10}]} = TimeSeries.all
+    %{"all1" => [{_time1, 55}, {_time2, 20}], "all2" => [{_time3, 45}, {_time4, 10}]} = TimeSeries.all
   end
 
   test "TimeSeries.clear returns :ok and erases all time series" do
-    TimeSeries.sample("clear_time_series1", 20)
-    TimeSeries.sample("clear_time_series1", 55)
-    TimeSeries.sample("clear_time_series2", 10)
-    TimeSeries.sample("clear_time_series2", 45)
+    TimeSeries.sample("clear1", 20)
+    TimeSeries.sample("clear1", 55)
+    TimeSeries.sample("clear2", 10)
+    TimeSeries.sample("clear2", 45)
 
     refute TimeSeries.all |> Enum.empty?
     TimeSeries.clear
@@ -31,12 +31,12 @@ defmodule Beaker.TimeSeriesTest do
   end
 
   test "TimeSeries.sample will return :ok and record the value for the time series at that point in time as well as keep it in chronologically descending order" do
-    assert TimeSeries.get("sample_series") == nil
+    assert TimeSeries.get("sample") == nil
 
-    :ok = TimeSeries.sample("sample_series", 10)
-    :ok = TimeSeries.sample("sample_series", 20)
+    :ok = TimeSeries.sample("sample", 10)
+    :ok = TimeSeries.sample("sample", 20)
 
-    [{time2, 20}, {time1, 10}] = TimeSeries.get("sample_series")
+    [{time2, 20}, {time1, 10}] = TimeSeries.get("sample")
 
     assert time2 > time1
   end
@@ -46,7 +46,7 @@ defmodule Beaker.TimeSeriesTest do
   end
 
   test "TimeSeries.get(key) returns the stored value in the time series" do
-    TimeSeries.sample("get_series", 10)
-    [{_time1, 10}] = TimeSeries.get("get_series")
+    TimeSeries.sample("get", 10)
+    [{_time1, 10}] = TimeSeries.get("get")
   end
 end
