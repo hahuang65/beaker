11f98321bf6c96db8ed90a41c159280cb48fc599
Howard Huang <hao@hwrd.me>
2015-12-29 18:47:54 -0800

    Version 1.1.3
    Enhancements
    ------------
      * Update dependencies to newest versions
      * Remove checking for presence of Phoenix and Ecto for their corresponding integrations
    
    Bug Fixes
    ---------
      * None
    
    Deprecations
    ------------
      * None
    
    Backwards Incompatible Changes
    ------------------------------
      * None

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Beaker.Mixfile do
   def project do
     [
       app: :beaker,
-      version: "1.1.2",
+      version: "1.1.3",
       elixir: "~> 1.0",
       elixirc_paths: elixirc_paths(Mix.env),
       compilers: compilers(Mix.env),
