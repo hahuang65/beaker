eb1937d53ddd94cd51fcb53c225161728fe5476d
Howard Huang <hao@hwrd.me>
2016-01-01 23:27:13 -0800

    Version 1.1.4
    Enhancements
    ------------
      * Update to Elixir 1.2
      * Change HashDict to Map
      * Update ex_doc
    
    Bug Fixes
    ---------
      * None
    
    Deprecations
    ------------
      * None
    
    Backwards Incompatible Changes
    ------------------------------
      * None

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Beaker.Mixfile do
   def project do
     [
       app: :beaker,
-      version: "1.1.3",
+      version: "1.1.4",
       elixir: "~> 1.2",
       elixirc_paths: elixirc_paths(Mix.env),
       compilers: compilers(Mix.env),
