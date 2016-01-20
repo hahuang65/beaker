7c7c4a6e7a2a2d8d88cbe72989bdb1b64e874143
Martin Feckie <mfeckie@gmail.com>
2016-01-20 18:11:52 +0800

    Change test data point name to avoid clashes

--- test/lib/beaker/formatters/aggregate_formatter_test.exs
+++ test/lib/beaker/formatters/aggregate_formatter_test.exs
@@ -3,18 +3,15 @@ defmodule Beaker.Formatters.AggregateTest do
 
   alias Beaker.TimeSeries
 
-  @expected_output %{
-  }
-
   setup do
     TimeSeries.Aggregated.clear
-    TimeSeries.sample("time_series1", 10)
-    TimeSeries.sample("time_series2", 20)
-    TimeSeries.sample("time_series3", 30)
+    TimeSeries.sample("aggregation_series1", 10)
+    TimeSeries.sample("aggregation_series2", 20)
+    TimeSeries.sample("aggregation_series3", 30)
 
-    TimeSeries.Aggregator.aggregate("time_series1", before_time: Beaker.Time.now + 5000, after_time: 0)
-    TimeSeries.Aggregator.aggregate("time_series2", before_time: Beaker.Time.now + 5000, after_time: 0)
-    TimeSeries.Aggregator.aggregate("time_series3", before_time: Beaker.Time.now + 5000, after_time: 0)
+    TimeSeries.Aggregator.aggregate("aggregation_series1", before_time: Beaker.Time.now + 5000, after_time: 0)
+    TimeSeries.Aggregator.aggregate("aggregation_series2", before_time: Beaker.Time.now + 5000, after_time: 0)
+    TimeSeries.Aggregator.aggregate("aggregation_series3", before_time: Beaker.Time.now + 5000, after_time: 0)
 
     on_exit fn ->
       TimeSeries.Aggregated.clear
@@ -27,13 +24,13 @@ defmodule Beaker.Formatters.AggregateTest do
 
     keys = Map.keys(mapped)
 
-    assert keys === ["time_series1", "time_series2", "time_series3"]
+    assert keys === ["aggregation_series1", "aggregation_series2", "aggregation_series3"]
 
-    time_series_1 = Map.get(mapped, "time_series1")
+    aggregation_series_1 = Map.get(mapped, "aggregation_series1")
 
-    assert is_list(time_series_1)
+    assert is_list(aggregation_series_1)
 
-    measurement = time_series_1 |> hd
+    measurement = aggregation_series_1 |> hd
 
     assert get_value(measurement) == %{average: 10.0, count: 1, max: 10, min: 10}
   end
