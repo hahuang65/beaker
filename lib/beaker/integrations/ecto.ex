a9d94f7618ee7426536b819771e8e2c74808db5a
Matt Widmann <mgwidmann@gmail.com>
2016-03-22 01:22:25 -0400

    Ecto 2.0 changes, removed log function and renamed to __log__

--- lib/beaker/integrations/ecto.ex
+++ lib/beaker/integrations/ecto.ex
@@ -3,15 +3,14 @@ defmodule Beaker.Integrations.Ecto do
 
   defmacro __using__(_opts) do
     quote do
-      def log(entry) do
+      defoverridable [__log__: 1]
+      def __log__(entry) do
         if entry.query_time, do: Beaker.TimeSeries.sample("Ecto:QueryTime", entry.query_time / 1000)
         if entry.queue_time, do: Beaker.TimeSeries.sample("Ecto:QueueTime", entry.queue_time / 1000)
         Beaker.Counter.incr("Ecto:Queries")
 
         super(entry)
       end
-
-      defoverridable [log: 1]
     end
   end
 end
